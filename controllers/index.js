81aaded86b0bb03ed1c156c62e9e166d50253cbc
wevexOP <111082615+wevexOP@users.noreply.github.com>
2022-12-20 20:26:24 -0800

    the server works now

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 // adding api routing here
 const { Router } =  require('express');
 
+const pathRouter = require('./pathRouter');
+
 const allRouter = new Router();
+
+allRouter.use('/', pathRouter);
 
 module.exports = allRouter;
