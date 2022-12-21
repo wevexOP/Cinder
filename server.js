3f399da0a7c991f94784af22b08275186f8c24f6
wevexOP <111082615+wevexOP@users.noreply.github.com>
2022-12-20 21:02:40 -0800

    adding more of the server routing as well as the handlebars views

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // This is going to be our server page. Backend ONLY!
 const express = require('express');
 
+const { engine } = require('express-handlebars');
+
 const mainRouter = require('./controllers');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 
 app.use(mainRouter);
 
