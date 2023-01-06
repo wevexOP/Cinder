99e6e41d09e3e646fc9345a4d895920c222298da
unknown <troysteedmiller@gmail.com>
2023-01-05 20:52:31 -0800

    socket revision part main.js

--- Socket2/public/js/main.js
+++ Socket2/public/js/main.js
@@ -1,6 +1,17 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+
+
+// Get username and room from url
+const { username, room }= Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+console.log(username, room);
+
 const socket = io();
+
+//join room
+socket.emit('joinRoom', {username, room})
 
 socket.on('message', message =>{
     console.log(message);
@@ -29,9 +40,9 @@ chatForm.addEventListener('submit', (e) =>{
 function outputMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-        ${message}
+        ${message.text}
     </p>`;
     document.querySelector('.chat-messages').appendChild(div);
 }
--- Socket2/server.js
+++ Socket2/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path =require('path');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app);
@@ -9,22 +10,27 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const adminName = 'Cinder Bot';
+
 // Run when a client connects
 io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room})=>{
+
+    });
     console.log('New WS Connection...');
-    socket.emit('message', 'Welcome to Cindir Chat!');
+    socket.emit('message', formatMessage(adminName, 'Welcome to Cindir Chat!'));
 
     //Shows when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message',formatMessage(adminName, 'A user has joined the chat'));
 
 //Runs when a user discconects
 socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
+    io.emit('message', formatMessage(adminName,'A user has left the chat'));
 });
 //Listen for msg
 socket.on("chatMessage", (msg) => {
-    io.emit('message', msg)
-});
+    io.emit('message', formatMessage('USER',msg))
+    });
 });
 const PORT = 4001 || process.env.PORT;
 
