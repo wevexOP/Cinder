9ab4d264d2cebc337d775901d19e60deb6ef6b9b
wevexOP <111082615+wevexOP@users.noreply.github.com>
2023-01-07 21:01:18 -0800

    tester code

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // This is going to be our server page. Backend ONLY!
+require('dotenv').config();
 const express = require('express');
-
 const { engine } = require('express-handlebars');
-
 const mainRouter = require('./controllers');
+const mysql = require('mysql2');
+
+
 
 const app = express();
 
@@ -20,4 +22,19 @@ app.use(mainRouter);
 
 app.listen(PORT, () => {
     console.log("Listening on http://localhost:" + PORT);
+});
+
+// Connection Poll
+const pool = mysql.createPoolCluster({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env,DB_NAME
+});
+
+// Connect to DB
+pool.getConnection((err, connection) => {
+    if(err) throw err; // not connected!
+    console.log('Connected as ID' + connection.threadID);
 });
