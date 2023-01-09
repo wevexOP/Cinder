46e8dc079e9be17a93ff4acacada97ce00801c94
kait <kingkittykait@gmail.com>
2023-01-09 01:40:08 -0800

    fixed db

--- controllers/db.js
+++ controllers/db.js
@@ -1,8 +1,10 @@
-const Sequalize = require('sequalize');
-const db = new Sequalize('devlove', 'root', '', {
+const Sequelize = require('sequelize');
+const db = new Sequelize('devlove', 'root', '', {
     host: "localhost",
     dialect: "mysql",
 
 });
+
+db.sync({});
 
 module.exports = db;
