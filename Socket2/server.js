8f9b29006b215ee2d73dfea5fb7607a387910d5b
unknown <troysteedmiller@gmail.com>
2023-01-09 17:43:24 -0800

    REDID SOCKETS FINAL

--- Socket2/server.js
+++ Socket2/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const http = require('http');
-const mysql = require('mysql2');
 const path =require('path');
+const http = require('http');
+const express = require('express');
+//const mysql = require('mysql2');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser} = require('./utils/user');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/user');
 
 
 
@@ -12,14 +12,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-let con = mysql.createConnection({
+/*let con = mysql.createConnection({
     host: "localhost",
     user: "",
     password: "",
     database: "devlove"
-  });
+  });*/
   
 
 const adminName = 'Cinder Bot';
@@ -44,7 +44,13 @@ io.on('connection', socket => {
         //Shows when a user connects
         socket.broadcast.to(user.room)
         .emit('message',formatMessage(adminName, `${user.username} has joined the chat`));
-    
+      
+
+        // send user room info
+        io.to(user.room).emit('roomUsers', {
+          room : user.room,
+          users: getRoomUsers(user.room)
+        });
     });
     console.log('New WS Connection...');
     
@@ -57,9 +63,16 @@ socket.on("chatMessage", (msg) => {
     });
     //Runs when a user discconects
 socket.on('disconnect', () => {
-    io.emit('message', formatMessage(adminName, `USER has left the chat`));
+const user = userLeave(socket.id);
+
+if (user) {
+  io.to(user.room).emit(
+    'message', formatMessage(adminName, `${user.username} has left the chat`));
+}
+
 });
 });
+//app.listen(process.env.PORT || 4001);
 const PORT = 4001 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
